566bd73b8f58c24b66aa787288577e64e8e52e46
Husam El <113930359+fudgeadool@users.noreply.github.com>
2023-01-26 18:51:20 +0000

    day 1

---
+++ main.py
@@ -0,0 +1,13 @@
+import random 
+numbers = [1,2,3,4,5]
+# print(numbers[0], "+" ,numbers[1])
+def math(numbers):
+  print("Hello! Welcome to this math game! You have to get 3 questions correct to win! Good luck!")
+  computer = random.choice(numbers)
+  print("Question #1:", computer, "+" , computer)
+
+
+math(numbers)
+
+# computer = random.choice(numbers)
+# print(computer, "+" , computer)
