0774669dec134ef1b1168d1fae63eb6dceb15b90
Husam El <113930359+fudgeadool@users.noreply.github.com>
2023-01-30 18:56:20 +0000

    day 2

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import random
 numbers = [1,2,3,4,5]
 # print(numbers[0], "+" ,numbers[1])
 def math(numbers):
-  print("Hello! Welcome to this math game! You have to get 3 questions correct to win! Good luck!")
-  computer = random.choice(numbers)
-  print("Question #1:", computer, "+" , computer)
-  ok =input("What is the answer?" )
-  if int == computer + computer:
-    print("Correct!")
-  else: 
-    print("WRONG")
-
-
+  print("Hello! Welcome to this addition math game! Good luck!")
+  ready = input("Ready?")
+  if ready != "yes":
+    exit()
+  while True:
+      computer = random.choice(numbers)
+      print("Question:", computer, "+" , computer)
+      answer = int(input("What is the answer?" ))
+      if answer == computer + computer:
+        print("Correct!")
+      else: 
+        print("WRONG")
+      Again = input("Do you want to play again? (yes or no) ")
+      if Again != "yes":
+        print("Goodbye!")
+        exit()
 math(numbers)
-
-# computer = random.choice(numbers)
-# print(computer, "+" , computer)
+  
+  # computer = random.choice(numbers)
+  # print(computer, "+" , computer)
+  
