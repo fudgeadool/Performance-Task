1b990dd57f32ad07eaa45a171ef414bad4c98af6
Husam El <113930359+fudgeadool@users.noreply.github.com>
2023-02-01 18:52:31 +0000

    Day 3

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-import random 
-numbers = [1,2,3,4,5]
-# print(numbers[0], "+" ,numbers[1])
-def math(numbers):
-  print("Hello! Welcome to this addition math game! Good luck!")
-  ready = input("Ready?")
-  if ready != "yes":
-    exit()
-  while True:
-      computer = random.choice(numbers)
-      print("Question:", computer, "+" , computer)
-      answer = int(input("What is the answer?" ))
-      if answer == computer + computer:
-        print("Correct!")
+import random #Randomizes the values in the list.
+numbers = [1,2,3,4,5] #List containing values that will be chosen at random for the addition questions.
+def math(numbers): #Function with 'numbers' as the parameter. 
+  print("Hello! Welcome to this addition math game! Good luck!") #States what the program will be
+  while True: #Loop to ask multiple questions if the user chooses to.
+      computer = random.choice(numbers) #Randomizes the chosen numbers from the list 'numbers'.
+      print("Question:", computer, "+" , computer) #Prints the question of the randomized number(computer) + the randomized number(computer).
+      answer = int(input("What is the answer?" )) #Asks the user what they think the answer is.
+      if answer == computer + computer: #If the user's 'answer", according to the computer, equals the randomized number(computer) + the randomized number(computer) it will either print 'correct' or incorrect.
+        print("Correct!") # If 'answer' is correct, prints 'correct'.
       else: 
-        print("WRONG")
-      Again = input("Do you want to play again? (yes or no) ")
-      if Again != "yes":
+        print("Incorrect") # If 'answer' is incorrect, prints 'incorrect'.
+      Again = input("Do you want to play again? (yes/no) ") #Asks the user if they want to play again.
+      if Again != "yes": #If users does not say 'yes' it will print 'goodbye' and end the loop/program. If user says 'yes' the loop will repeat and print another randomized question.
         print("Goodbye!")
         exit()
-math(numbers)
-  
-  # computer = random.choice(numbers)
-  # print(computer, "+" , computer)
-  
+math(numbers) #Calls the function.
